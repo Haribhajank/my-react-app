2bf91310364b966180279762bb1d2c3e58732520
Haribhajan <k.haribhajan@iitg.ac.in>
2024-03-20 01:37:40 +0530

    Updated Commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="container">
+      <div className="button-container">
+      <button className="big-button">
+          <img src="/music.webp" alt="Button 1" />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className="big-button">
+          <img src="/OIP.jpeg" alt="Button 2" />
+        </button>
+        <button className="big-button">
+          <img src="/phone_number-512.webp" alt="Button 3" />
+        </button>
+        <button className="big-button">
+          <img src="/R.png" alt="Button 4" />
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
